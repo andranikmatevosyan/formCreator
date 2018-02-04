04ad46b902c2155306c86024a9fd31830d62f9c3
andranikmatevosyan <36126253+andranikmatevosyan@users.noreply.github.com>
2018-02-04 16:04:15 +0400

    Update init.php

--- init.php
+++ init.php
@@ -12,15 +12,6 @@ $smarty->debugging = false;
 $smarty->caching = false;
 $smarty->cache_lifetime = 120;
 $smarty->assign("activeClass", 'active', true);
-/*
-
-$smarty->assign("currentLanguage", $currentLanguage, true);
-$smarty->assign("actualLink", $actualLink, true);
-
-foreach ($lang as $langKey => $langString){
-	$smarty->assign('lang_'.$langKey, $lang[$langKey], true);
-}
-
 $action = $_GET['action'];
-*/
+
 ?>
