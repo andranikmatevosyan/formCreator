42e3499fdf5c48ebeff3e49641fc38a3c7ab3bd5
andranikmatevosyan <36126253+andranikmatevosyan@users.noreply.github.com>
2018-02-04 16:04:35 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'init.php';
-//require_once 'config.php';
 $forms = getMoreDB("SELECT id, formName, formId, formClass, formAction, formMethod FROM form_general ORDER BY id DESC");
 
 $smarty->assign("filename", "homepage", true);
